0850968188faebbffc9009700984ef5d7509b389
Jaemin Ahn <jae7415@gmail.com>
2019-10-28 20:29:49 +0900

    Add Redux and Fix Compile Errors

---
+++ web/src/store/index.ts
@@ -0,0 +1,35 @@
+import {
+  applyMiddleware,
+  combineReducers,
+  createStore,
+  Store,
+  compose
+} from "redux";
+import thunk from "redux-thunk";
+
+declare global {
+  // Add Redux Dev Tool Property into Window Object
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: typeof compose;
+  }
+}
+
+const rootReducer = combineReducers({});
+
+let store: Store;
+export const configureStore = (): Store => {
+  const middleware = [];
+
+  middleware.push(thunk);
+  if (process.env.NODE_ENV !== "production") {
+    const composeEnhancers =
+      (typeof window != "undefined" &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+      compose;
+    store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk))); //middleWare 추가
+  } else {
+    store = createStore(rootReducer, applyMiddleware(thunk));
+  }
+
+  return store;
+};
